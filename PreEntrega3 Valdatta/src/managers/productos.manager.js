9c998c1b677e4fb411d5bdc4efcd5d9f28e62e21
ValenValdatta <valenin.valdatta.94@gmail.com>
2024-01-12 10:40:45 -0300

    2do commit de la pre entrega 3 del curso de JS le saque una s en la linea 7 del archivo de productos manager para que funcione correctamente la app y le saque una funcion que estaba comentada

--- PreEntrega3 Valdatta/src/managers/productos.manager.js
+++ PreEntrega3 Valdatta/src/managers/productos.manager.js
@@ -4,7 +4,7 @@ import { crearProducto } from "../models/productos.model.js"
 import htmlElements from "../elements/html.elements";
 
 // tengo que crear un array donde se van a depositar los productos que cree con mi funcion agregar producto
-let productos = JSON.parse(localStorage.getItem("productos")) || [];
+let productos = JSON.parse(localStorage.getItem("producto")) || [];
 
 //arriba lo que hice fue hacer que el texto JSON pueda verse como un array en la consola, caso contrario me va a mostrar un array vacio
 
@@ -67,42 +67,6 @@ const eliminarProducto = (idProducto) => {
     mostrarProductos(); 
 
 }
-
-// const mostrarProductosCarrito = () => {
-    
-//     htmlElements.contenedorProductos.innerHTML = "";
-//     console.log(productos);
-
-//     productos.forEach(producto => {
-//         let tarjeta = document.createElement("div")
-//         tarjeta.className = "border border-3 border-dark rounded-2 p-2 bg-secondary d-flex justify-content-between m-2"
-//         tarjeta.innerHTML = `
-//                             <p class="mt-1">Producto: ${producto.data}</p
-//                             <p class="mt-1">Precio: ${producto.precio}</p>
-//                             `
-
-//         let contenedorBtn = document.createElement("div")
-
-//         let btnEliminar = document.createElement("button")
-//         btnEliminar.innerText = "Eliminar";
-//         btnEliminar.className = "rounded-2 bg-danger border-2 m-1"
-//         //despues de agregar el boton eliminar, le sumo un evento on click para que cuando haga click elimine un producto tomando un ID
-//         btnEliminar.onclick = () => eliminarProducto(producto.id);
-
-        
-        
-//         tarjeta.appendChild(contenedorBtn)
-//         contenedorBtn.appendChild(btnEliminar)
-//         contenedorBtn.appendChild(btnComprar)
-        
-        
-
-
-
-//         htmlElements.carritoProductos.appendChild(tarjeta)
-//     });
-// }
-
 
 
 export default {
