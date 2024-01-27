2370ce6ed8ebf7385a7a05d54ee6f24fbcd9c7c5
Valen <valentin.valdatta.94@gmail.com>
2024-01-27 17:03:31 -0300

    agregue libreria uuid al proyecto

--- PreEntrega3 Valdatta/src/models/productos.model.js
+++ PreEntrega3 Valdatta/src/models/productos.model.js
@@ -1,8 +1,10 @@
 //en la parte de models irian los MODELOS DE ETIQUETA DE PRODUCTOS que vamos a ir agregando
+import { v4 } from "uuid";
+
 
 export const crearProducto = (texto, precio) => {
     let nuevoProducto = {
-        id: Date.now().toString(36),
+        id: v4(),
         data: texto,
         precio: precio,
     };
